4ed30cf2e8255776e4db33dd0866c1556d44e22e
N00rAhmed <noor.ahmed232003@outlook.com>
2025-08-18 20:08:45 +0100

    first commit to awesome game

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is gonna be a 2d side scroll action game");
+		System.out.println("it will have a character in a world with things chasing after it");
+	}
+
+}
