55ebad0fc04f139cb11ade26b7089ab180cdf67c
N00rAhmed <noor.ahmed232003@outlook.com>
2025-08-19 19:40:36 +0100

    made a window

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("this is gonna be a 2d side scroll action game");
 		System.out.println("it will have a character in a world with things chasing after it");
+		
+		Window window = new Window();
+		
 	}
 
 }
---
+++ src/Window.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class Window {
+	
+	JFrame frame = new JFrame();
+	
+	public Window() {
+		
+		System.out.println("window");
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(1000,700);
+		frame.setLayout(null);
+		frame.setVisible(true);
+		
+	}
+}
