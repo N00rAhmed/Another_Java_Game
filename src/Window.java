36f8555704d8a5d1dbc33148e99ec76dae1a1790
N00rAhmed <noor.ahmed232003@outlook.com>
2025-08-22 20:58:47 +0100

    removed some lines of code

--- src/Window.java
+++ src/Window.java
@@ -10,23 +10,6 @@ public class Window {
 		
 	public Window() {
 		
-//		String imagePath = "C:\\\\Users\\\\Noor\\\\Desktop\\\\PROGRAMMING\\\\Epic_Game\\\\Cool_Java_Game\\\\src\\\\mario.png";
-//	
-//        frame = new JFrame("Image Display");
-//        var icon = new ImageIcon(imagePath);
-//
-//        if (icon.getImageLoadStatus() != java.awt.MediaTracker.COMPLETE) {
-//            System.out.println("Image failed to load.");
-//            return;
-//        }
-//
-//        Image img = icon.getImage();
-//        Image resizedImage = img.getScaledInstance(600, 600, java.awt.Image.SCALE_SMOOTH);
-//        icon = new ImageIcon(resizedImage);
-//
-//        var label = new JLabel(icon);
-//        frame.add(label);
-
 		
 		
 		
@@ -41,12 +24,6 @@ public class Window {
 		
 	}
 	
-//    public void show() {
-//        frame.setVisible(true);  // Makes the frame visible
-//    }
-//    public static void main(String[] args) {
-//        new Window();
-//    }
 
 	
 }
