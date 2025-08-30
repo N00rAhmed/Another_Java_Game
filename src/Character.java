b6f8f5c03f97e5fc645f8cf70da6f050441e677a
N00rAhmed <noor.ahmed232003@outlook.com>
2025-08-30 21:00:58 +0100

    removed code

--- src/Character.java
+++ src/Character.java
@@ -13,15 +13,11 @@ public class Character extends JFrame{
 	public Character() {
 		
 		System.out.println("Character Class");
-
+		
 		String imagePath = "C:\\\\Users\\\\Noor\\\\Desktop\\\\PROGRAMMING\\\\Epic_Game\\\\Cool_Java_Game\\\\src\\\\mario.png";
-
+		
 		var icon = new ImageIcon(imagePath);
 
-        if (icon.getImageLoadStatus() != java.awt.MediaTracker.COMPLETE) {
-            System.out.println("Image failed to load.");
-            return;
-        }
         
 //        figure out controlls and then rendering an image maybe to make it easier have a look at the tutorials you used before from that guy making
 //        a java game
